91007721e1d1d3361ce2ae231a1fc34a69236f27
PoornaManakal <166126968+PoornaManakal@users.noreply.github.com>
2024-11-13 11:21:27 +0530

    project started
    backend created

---
+++ ticket-pool/src/main/java/com/OOP/ticket_pool/TicketPoolApplication.java
@@ -0,0 +1,13 @@
+package com.OOP.ticket_pool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketPoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketPoolApplication.class, args);
+	}
+
+}
---
+++ ticket-pool/src/test/java/com/OOP/ticket_pool/TicketPoolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.OOP.ticket_pool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketPoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
